9751e59c7b43fff59191535680dc9dea82554e1c
claudym <claudy.m.mejia@gmail.com>
2022-09-14 09:30:46 -0400

    add hash_password and check_password utilities

---
+++ utils.py
@@ -0,0 +1,8 @@
+from passlib.hash import pbkdf2_sha256
+
+
+def hash_password(password):
+    return pbkdf2_sha256.using(rounds=100000, salt_size=32).hash(password)
+
+def check_password(password, hashed):
+    return pbkdf2_sha256.verify(password, hashed)
