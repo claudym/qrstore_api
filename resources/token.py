7a485fd65b3bf457fd2d75e7354788b291d41103
claudym <claudy.m.mejia@gmail.com>
2022-09-14 12:21:13 -0400

    import create_refresh_token, jwt_required, get_jwt_identity, and add refresh token resource

--- resources/token.py
+++ resources/token.py
@@ -1,7 +1,12 @@
 from http import HTTPStatus
 from flask import request
 from flask_restful import Resource
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity
+)
 from utils import check_password
 from models.user import User
 
@@ -14,5 +19,14 @@ class TokenResource(Resource):
         if user is None or not check_password(password, user.hashed):
             return {'message': 'email or password is incorrect'}, HTTPStatus.UNAUTHORIZED
         
-        access_token = create_access_token(identity=user.id)
+        access_token = create_access_token(identity=user.id, fresh=True)
+        refresh_token = create_refresh_token(identity=user.id)
+        return {'access_token': access_token, 'refresh_token': refresh_token}
+
+
+class RefreshTokenResource(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity=current_user, fresh=False)
         return {'access_token': access_token}
