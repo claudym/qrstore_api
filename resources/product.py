0a9c34647e111e89ca302535ddfcd7f95571c36b
claudym <claudy.m.mejia@gmail.com>
2022-09-14 17:19:20 -0400

    import from flask_jwt_extended, marshmallow, schemas.product and add post to ProductListResource

--- resources/product.py
+++ resources/product.py
@@ -1,20 +1,41 @@
 from flask import request
 from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from http import HTTPStatus
+from marshmallow import ValidationError
+from sqlalchemy.exc import DatabaseError
 from models.product import Product
+from schemas.product import ProductSchema
+
+
+product_schema = ProductSchema()
+product_list_schema = ProductSchema(many=True)
 
 
 class ProductListResource(Resource):
     def get(self):
         products = Product.get_all()
-        print(products) # test
-
-        return {'data': products}
+        return product_list_schema.dump(products)
     
-    # def post(self):
+    @jwt_required()
+    def post(self):
+        json_data = request.get_json()
+        current_user = get_jwt_identity()
+        try:
+            data = product_schema.load(data=json_data)
+        except ValidationError as err:
+            return {'message': 'Validation errors', 'errors': err.messages}, HTTPStatus.BAD_REQUEST
+
+        product = Product(**data)
+        product.user_id = current_user
+        
+        try:
+            product.save()
+        except DatabaseError as err:
+            return {'message': 'Validation errors', 'errors': str(err.orig)}, HTTPStatus.BAD_REQUEST
+        return product_schema.dump(product), HTTPStatus.CREATED
 
 class ProductResource(Resource):
     def get(self, product_id):
         product = Product.get_by_id(product_id)
-        print(product) # test
         return {'data': product}, HTTPStatus.OK
