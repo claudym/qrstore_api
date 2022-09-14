caa1b97bb6a5b310628a84d63a9c95fd4f48eb61
claudym <claudy.m.mejia@gmail.com>
2022-09-14 09:46:35 -0400

    add JWT_ERROR_MESSAGE_KEY to config

--- config/Config.py
+++ config/Config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    JWT_ERROR_MESSAGE_KEY = 'message'
 
 class DevelopmentConfig(Config):
     DEBUG = True
