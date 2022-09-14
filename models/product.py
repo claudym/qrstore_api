7f8608bc1fbe70a951d0fc56416eef87934b0603
claudym <claudy.m.mejia@gmail.com>
2022-09-14 17:05:21 -0400

    make desc unique and add user_id field

--- models/product.py
+++ models/product.py
@@ -3,10 +3,11 @@ from extensions import db
 class Product(db.Model):
     __tablename__ = 'product'
     id = db.Column(db.Integer, primary_key=True)
-    desc = db.Column(db.String(100), nullable=False)
+    desc = db.Column(db.String(100), nullable=False, unique=True)
     price = db.Column(db.Numeric(), nullable=False)
     size = db.Column(db.String(50))
     image = db.Column(db.String(100), default=None)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
     updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now(), onupdate=db.func.now())
     # created_by
