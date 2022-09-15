1ba5a75ecdbc8a4ff1a4f77f88326d3466c14f7f
claudym <claudy.m.mejia@gmail.com>
2022-09-15 09:50:44 -0400

    create role model

---
+++ models/role.py
@@ -0,0 +1,11 @@
+from extensions import db
+
+
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True)
+    desc = db.Column(db.String(30), nullable=False, unique=True)
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.filter_by(id=id).first()
