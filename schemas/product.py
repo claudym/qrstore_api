18d382de3afa89765b1ae93a14ad5272948d0a55
claudym <claudy.m.mejia@gmail.com>
2022-09-16 12:54:25 -0400

    add sex to product schema

--- schemas/product.py
+++ schemas/product.py
@@ -9,7 +9,8 @@ class ProductSchema(Schema):
     id = fields.Int(dump_only=True)
     desc = fields.Str(required=True, validate=[validate.Length(max=100)])
     price = fields.Decimal(required=True, as_string=True)
-    size = fields.Str()
+    size = fields.Str(required=True)
+    sex = fields.Str(required=True)
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
     user = fields.Nested(
