725959b5642a868dabc3cbca9e071e4f1fbb0c54
claudym <claudy.m.mejia@gmail.com>
2022-09-15 22:02:35 -0400

    import user model, add get and delete from ProductResource

--- resources/product.py
+++ resources/product.py
@@ -4,6 +4,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from http import HTTPStatus
 from marshmallow import ValidationError
 from sqlalchemy.exc import DatabaseError
+from models.user import User
 from models.product import Product
 from schemas.product import ProductSchema
 
@@ -13,6 +14,7 @@ product_list_schema = ProductSchema(many=True)
 
 
 class ProductListResource(Resource):
+    @jwt_required()
     def get(self):
         products = Product.get_all()
         return product_list_schema.dump(products)
@@ -43,6 +45,27 @@ class ProductListResource(Resource):
 
 
 class ProductResource(Resource):
+    @jwt_required()
     def get(self, product_id):
         product = Product.get_by_id(product_id)
-        return {"data": product}, HTTPStatus.OK
+        if product is None:
+            return {"message": "Product not found"}, HTTPStatus.NOT_FOUND
+        return product_schema.dump(product)
+
+    @jwt_required()
+    def delete(self, product_id):
+        product = Product.get_by_id(product_id)
+        if product is None:
+            return {"message": "Product not found"}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+        if User.get_by_id(current_user).role_id >= 3:  # 1-Admin, 2-Manager, 3-user
+            return {"message": "Access is not allowed"}, HTTPStatus.FORBIDDEN
+
+        product.delete()
+        return {}, HTTPStatus.NO_CONTENT
+
+    @jwt_required()
+    def patch(self, product_id):
+        # current_user = get_jwt_identity()
+        pass
