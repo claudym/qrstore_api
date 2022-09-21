9773f2d3396ecf64222c677c5600b84dfadfcea3
claudym <claudy.m.mejia@gmail.com>
2022-09-21 02:24:42 -0400

    create order_item schema

---
+++ schemas/order_item.py
@@ -0,0 +1,17 @@
+from marshmallow import Schema, fields, validates, ValidationError
+
+
+class OrderItemSchema(Schema):
+    class Meta:
+        ordered = True
+
+    id = fields.Int(dump_only=True)
+    order_id = fields.Int(dump_only=True)
+    product_id = fields.Int(load_only=True)
+    product_snapshot_id = fields.Int(dump_only=True)
+    count = fields.Int(required=True)
+
+    @validates("count")
+    def validate_count(self, value):
+        if value < 0:
+            raise ValidationError("Count cannot be negative")
