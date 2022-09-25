3e56a1974a61b29052551022429fa37e406f6e1d
claudym <claudy.m.mejia@gmail.com>
2022-09-25 00:27:06 -0400

    update config

--- config/Config.py
+++ config/Config.py
@@ -5,6 +5,8 @@ class Config:
     DEBUG = False
     SECRET_KEY = os.environ.get("SECRET_KEY")
     JWT_ERROR_MESSAGE_KEY = "message"
+    UPLOADED_PHOTOS_DEST = "static"
+    MAX_CONTENT_LENGTH = 10 * 1024 * 1024
 
 
 class DevelopmentConfig(Config):
